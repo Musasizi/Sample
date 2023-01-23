650d48e98b7c4e65007bc6557108f8b3aca1c5e5
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-01-23 15:39:05 +0300

    Understanding variables

---
+++ variables.py
@@ -0,0 +1,59 @@
+
+# print('Hello, world')
+
+
+# Variables in Python
+# Python is type inferred
+
+name = "Jane Doe"
+age = 16
+gender = "female😊"
+marital_status = "single"
+
+homeDistrict = "Kampala"
+home_country = "Congo"
+
+
+PI = 3.14
+SPEED_OF_LIGHT = 30000
+
+print(name)
+print(age)
+print(gender)
+print(marital_status)
+
+# Rules of declaring variables
+# 1 Naming variables
+
+# snake_case
+# camelCase
+
+# MACRO_CASE
+# CapsCase
+
+# 2 DON'T DO THIS - Avoid using Keywords to declare variables
+# if = "DRC"
+
+
+# LITERAL
+# Numeric literals ]
+amount = 5000
+distance = 10.5
+quantity = 0.5
+balance = -9000
+
+# String literals
+student_name = "Seth"
+regNo = "S21B22/000"
+
+# Boolean literals
+isResident = True
+hasMeals = False
+
+# Special literals
+graduation_date = None
+
+# Collection literals
+semester_studied = [1, 2, "Three"]
+
+cart_item = ["Book", "Bic Pencil", "Sugar", "Coffee"]

552aed7b5cfea96320970b7302265bdcda80a153
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-01-30 15:11:51 +0300

    ✅functions

---
+++ functions/functions.py
@@ -0,0 +1,32 @@
+'''
+
+FUNCTION
+A function in Python is a reusable block of code that performs a specific task and returns a result.
+
+
+TYPES OF FUNCTIONS
+
+There are two types of function in Python programming:
+    ✅Standard library functions - These are built-in functions in Python that are available to use.
+    ✅User-defined functions - We can create our own functions based on our requirements.
+
+Other types of functions
+    ✅Anonymous functions (Lambda functions): Small, single-expression functions that can be created without using the def keyword.
+    ✅Recursive functions: Functions that call themselves until a base case is reached.
+    ✅Higher-order functions: Functions that accept other functions as arguments or return functions as outputs. 
+
+'''
+
+'''
+print(): used to display output on the console
+len(): used to get the length of an object (e.g. string, list, tuple, etc.)
+int(): used to convert a string or float to an integer
+str(): used to convert an object to a string
+max(): used to find the maximum value in a list or tuple
+min(): used to find the minimum value in a list or tuple
+sum(): used to find the sum of values in a list or tuple
+sorted(): used to sort a list or tuple
+range(): used to generate a sequence of numbers
+abs(): used to find the absolute value of a number.
+
+'''
